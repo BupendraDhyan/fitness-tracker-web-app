5f0f810e94e5c948382b9d4c4d6e6b64e8ef9794
BupendraDhyan <bddwijegunawardhana@gmail.com>
2023-07-01 12:04:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,35 @@ class Login extends Component {
                 }
 
             );
-        } else {}
+        } else {
+            this.setState({
+                loading: false
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="col-md-12">
+                <div className="card card-container">
+                    <img
+                        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                        alt="profile-img"
+                        className="profile-img-card"
+                        />
+                    
+                    <Form
+                        onSubmit={this.handleLogin}
+                        ref={c => {
+                            this.form = c;
+                        }
+                        }
+                        >
+                            
+                        </Form>
+                </div>
+            </div>
+        )
     }
 
 }
